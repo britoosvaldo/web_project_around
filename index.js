17903415d81e568db58dbd385928c0ac4f28deb6
Osvaldobrito <osvaldooabn@gmail.com>
2025-03-31 20:57:52 -0400

    Atualização do projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+const editButton = document.querySelector(".profile__edit-button");
+editButton.addEventListener("click", function () {
+  document.querySelector(".popup").classList.add("popup__opened");
+});
+
+const closeButton = document.querySelector(".popup__close-button");
+closeButton.addEventListener("click", function () {
+  document.querySelector(".popup").classList.add("popup__closed");
+});
