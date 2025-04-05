4a13521e73d448ca292cd9dd699a38f4333e740e
Osvaldobrito <osvaldooabn@gmail.com>
2025-04-05 12:32:47 -0400

    atualização do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@ const closeButton = document.querySelector(".popup__close-button");
 closeButton.addEventListener("click", function () {
   document.querySelector(".popup").classList.remove("popup__opened");
 });
+
+let formElement = document.querySelector(".popup__form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector("#name").value;
+  let aboutInput = document.querySelector("#about").value;
+
+  let userName = document.querySelector(".profile__name");
+  let userAbout = document.querySelector(".profile__about");
+
+  userName.textContent = nameInput;
+  userAbout.textContent = aboutInput;
+
+  document.querySelector(".popup").classList.remove("popup__opened");
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
