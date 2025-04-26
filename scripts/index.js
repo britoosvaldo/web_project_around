4ff23b5e085977795f1b9575b8ce27994dab696c
Osvaldobrito <osvaldooabn@gmail.com>
2025-04-26 12:54:37 -0400

    atualização do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 const editButton = document.querySelector(".profile__edit-button");
 editButton.addEventListener("click", function () {
-  document.querySelector(".popup").classList.add("popup__opened");
+  document.querySelector(".edit-popup").classList.add("edit-popup__opened");
 });
 
-const closeButton = document.querySelector(".popup__close-button");
-closeButton.addEventListener("click", function () {
-  document.querySelector(".popup").classList.remove("popup__opened");
+const editCloseButton = document.querySelector(".edit-popup__close-button");
+editCloseButton.addEventListener("click", function () {
+  document.querySelector(".edit-popup").classList.remove("edit-popup__opened");
 });
 
-const formElement = document.querySelector(".popup__form");
+const formElement = document.querySelector(".edit-popup__form");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -22,7 +22,17 @@ function handleProfileFormSubmit(evt) {
   userName.textContent = nameInput;
   userAbout.textContent = aboutInput;
 
-  document.querySelector(".popup").classList.remove("popup__opened");
+  document.querySelector(".edit-popup").classList.remove("edit-popup__opened");
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+const addButton = document.querySelector(".profile__add-button");
+addButton.addEventListener("click", function () {
+  document.querySelector(".add-popup").classList.add("add-popup__opened");
+});
+
+const addCloseButton = document.querySelector(".add-popup__close-button");
+addCloseButton.addEventListener("click", function () {
+  document.querySelector(".add-popup").classList.remove("add-popup__opened");
+});
