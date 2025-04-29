cf86ae5c915cb1f6282350db1770f8f1d33a7cb3
Osvaldobrito <osvaldooabn@gmail.com>
2025-04-29 16:41:26 -0400

    atualização do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// edit button
+
 const editButton = document.querySelector(".profile__edit-button");
 editButton.addEventListener("click", function () {
   document.querySelector(".edit-popup").classList.add("edit-popup__opened");
@@ -27,6 +29,8 @@ function handleProfileFormSubmit(evt) {
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
 
+// add button
+
 const addButton = document.querySelector(".profile__add-button");
 addButton.addEventListener("click", function () {
   document.querySelector(".add-popup").classList.add("add-popup__opened");
@@ -35,4 +39,69 @@ addButton.addEventListener("click", function () {
 const addCloseButton = document.querySelector(".add-popup__close-button");
 addCloseButton.addEventListener("click", function () {
   document.querySelector(".add-popup").classList.remove("add-popup__opened");
+});
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+const container = document.querySelector(".elements");
+
+initialCards.forEach(({ name, link }) => {
+  const card = document.createElement("div");
+  card.classList.add("elements__card");
+
+  const deleteButton = document.createElement("img");
+  deleteButton.src = "images/delete-button.png"; // coloque o caminho correto do ícone de lixeira
+  deleteButton.alt = "Deletar";
+  deleteButton.classList.add("elements__delete-button");
+
+  const image = document.createElement("img");
+  image.classList.add("elements__image");
+  image.src = link;
+  image.alt = name;
+
+  const description = document.createElement("div");
+  description.classList.add("elements__description");
+
+  const title = document.createElement("h2");
+  title.classList.add("elements__text");
+  title.textContent = name;
+
+  const likeButton = document.createElement("img");
+  likeButton.classList.add("elements__like-button");
+  likeButton.src = "images/like-button.png"; // coloque o caminho correto do ícone de like
+  likeButton.alt = "Curtir";
+
+  description.appendChild(title);
+  description.appendChild(likeButton);
+
+  card.appendChild(deleteButton);
+  card.appendChild(image);
+  card.appendChild(description);
+
+  container.appendChild(card);
 });
